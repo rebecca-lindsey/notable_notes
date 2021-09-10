1e6665f6406fa267147264bdae0fa07c88258b59
Rebecca <rebecca.hickson@podium.com>
2021-09-09 20:05:01 -0400

    Remove UserController index, add new and create

--- lib/notable_notes_web/controllers/user_controller.ex
+++ lib/notable_notes_web/controllers/user_controller.ex
@@ -1,7 +1,10 @@
 defmodule NotableNotesWeb.UserController do
   use NotableNotesWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(_conn, _params) do
   end
 end
