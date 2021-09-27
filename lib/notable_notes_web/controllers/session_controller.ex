f00a358f8173b1193cbbaf9f3adce3bd29bdbf1a
Rebecca <rebecca.hickson@podium.com>
2021-09-26 23:16:40 -0400

    Add create session

--- lib/notable_notes_web/controllers/session_controller.ex
+++ lib/notable_notes_web/controllers/session_controller.ex
@@ -1,11 +1,30 @@
 defmodule NotableNotesWeb.SessionController do
   use NotableNotesWeb, :controller
 
+  alias NotableNotes.Session
+  alias NotableNotes.Auth.Guardian
+
   def new(conn, _params) do
     render(conn, "new.html")
   end
 
-  def create(_conn, _params) do
+  def create(conn, params) do
+    case Session.authenticate(params) do
+      {:ok, %{username: username} = user} ->
+        conn
+        |> Guardian.Plug.sign_in(user)
+        |> put_flash(:info, "Logged in as #{username}!")
+        |> render("new.html")
+
+      {:error, message} ->
+        conn
+        |> put_flash(:error, message)
+        |> render("new.html")
+    end
+
+    conn
+    |> put_flash(:error, "Unable to login!")
+    |> render("new.html")
   end
 
   def delete(_conn, _params) do
